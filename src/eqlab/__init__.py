97becff177196e6205caa489884be0b399c1086e
betaanoiar1-gif <betaanoiar1@gmail.com>
2026-08-25 01:39:03 +0100

    feat: export selection engine from package root

--- src/eqlab/__init__.py
+++ src/eqlab/__init__.py
@@ -1,3 +1,14 @@
 """Evolutionary Quant Strategy Laboratory."""
 
+from .dna import StrategyDNA
+from .selection import SelectionCandidate, SelectionConfig, SelectionEngine, SelectionReport
+
 __version__ = "0.1.0"
+
+__all__ = [
+    "StrategyDNA",
+    "SelectionCandidate",
+    "SelectionConfig",
+    "SelectionEngine",
+    "SelectionReport",
+]
