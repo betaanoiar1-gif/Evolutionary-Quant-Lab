d842a58c74c34e7df6571c240d7b823cfaa0c07b
betaanoiar1-gif <betaanoiar1@gmail.com>
2026-08-25 01:43:34 +0100

    fix: export Laboratory from package root

--- src/eqlab/__init__.py
+++ src/eqlab/__init__.py
@@ -1,12 +1,15 @@
 """Evolutionary Quant Strategy Laboratory."""
 
 from .dna import StrategyDNA
+from .pipeline import Laboratory, ResearchResult
 from .selection import SelectionCandidate, SelectionConfig, SelectionEngine, SelectionReport
 
 __version__ = "0.1.0"
 
 __all__ = [
     "StrategyDNA",
+    "Laboratory",
+    "ResearchResult",
     "SelectionCandidate",
     "SelectionConfig",
     "SelectionEngine",
